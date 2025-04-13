78a5a8c8aa72dc1e602498c53eb98cf8401ac3be
stalker7521 <yaristalker@gmail.com>
2025-04-13 13:27:44 +0500

    adding function which append metres with date

--- bot.py
+++ bot.py
@@ -9,10 +9,10 @@ CREDS_FILE = './creds/so_test.json'  # Путь к файлу credential.json
 # Инициализация бота
 bot = telebot.TeleBot(TOKEN)
 
-# /todo захостить бота и сделать его автономным
-# /todo еще есть идея создать команду, которая будет позволять пользователю записывать метры за старые даты,
-#  то есть команда дата +метры: 10.04 +1000
 
+# /todo захостить бота и сделать его автономным
+
+# /todo добавить логгирование бота
 # Функция для подключения к Google Sheets
 def get_gsheet_client():
     creds = ServiceAccountCredentials.from_json_keyfile_name(CREDS_FILE, SCOPE)
@@ -21,20 +21,21 @@ def get_gsheet_client():
 
 
 # Функция для записи данных в Google Sheets
-def write_to_sheet(value, usr_id):
+def write_to_sheet(value, usr_id, date):
     """Берем текущую дату"""
-    today = datetime.now().strftime("%d.%m.%Y")  # -> "13.04.2025"
+    if date == "":
+        date = datetime.now().strftime("%d.%m.%Y")  # -> "13.04.2025"
 
     try:
         client = get_gsheet_client()
         sheet = client.open_by_key(SPREADSHEET_ID).worksheet(WORKSHEET_NAME)
 
-        """Ищем строку с сегодняшней датой"""
-        dates = sheet.col_values(1)  # Получаем все даты из столбца A
+        """Ищем строку с указанной датой"""
+        dates = sheet.col_values(1)  # Получаем все даты из столбца A (он с датами)
 
         if user_column_map[usr_id]:
             column = user_column_map[usr_id]  # вытаскиваем из словаря столбец пользователя по его tg-id
-            row_num = dates.index(today) + 1  # +1 т.к. нумерация с 1
+            row_num = dates.index(date) + 1  # +1 т.к. нумерация с 1
             sheet.update_cell(row_num, column, value)  # добавляем в последнюю ячейку определенного столбца данные
             print(f'Value "{value}" appended to sheet')
 
@@ -47,11 +48,27 @@ def write_to_sheet(value, usr_id):
 def handle_number_message(message):
     number = message.text[1:]
     user_id = str(message.from_user.id)
+    date = ""
     print(f'ID пользователя, который ввел данные: {user_id}')
-    write_to_sheet(number, user_id)  # записываем число в таблицу
+    write_to_sheet(number, user_id, date)  # записываем число в таблицу
     # list_of_data.append(number)  # добавляем число в список
     # count_of_dist += int(number)  # увеличиваем общий счетчик
     bot.reply_to(message, f'Number {number} has been recorded.')
+
+
+# Обработчик сообщений вида: +метры дата_куда_нужно_записать_метры
+@bot.message_handler(
+    func=lambda message: message.text.startswith('+') and message.text.split()[0][1:].isdigit() and len(
+        message.text.split()) == 2)
+def handle_number_with_data_message(message):
+    number = message.text.split()[0][1:]
+    date = str(message.text.split()[1])
+    user_id = str(message.from_user.id)
+    print(f'ID пользователя, который ввел данные: {user_id}')
+    write_to_sheet(number, user_id, date)  # записываем число в таблицу
+    # list_of_data.append(number)  # добавляем число в список
+    # count_of_dist += int(number)  # увеличиваем общий счетчик
+    bot.reply_to(message, f'Number {number} has been recorded on the date {date}')
 
 
 # @bot.message_handler(commands=['list'])
